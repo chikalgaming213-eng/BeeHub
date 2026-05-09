9472de9da66378b154058b31c8cd8674e1887209
chikalgaming213-eng <chikalgaming213@example.com>
2026-05-09 21:51:23 +0700

    fix: perbaiki raw path di loader

--- loader.lua
+++ loader.lua
@@ -1,20 +1,11 @@
--- Loader Bee Hub - Execute ini di Delta
-local repoUrl = "https://github.com/chikalgaming213-eng/BeeHub.git"
--- Ganti YOUR_USERNAME dan YOUR_REPO_NAME sesuai repo lu
+local repoUrl = "https://raw.githubusercontent.com/chikalgaming213-eng/BeeHub/main/BeeHub/src/"
 
--- Load config
 loadstring(game:HttpGet(repoUrl .. "config.lua"))()
 wait(0.5)
-
--- Load lib
 loadstring(game:HttpGet(repoUrl .. "lib.lua"))()
 wait(0.5)
-
--- Load GUI
 loadstring(game:HttpGet(repoUrl .. "gui.lua"))()
 wait(0.5)
-
--- Load main
 loadstring(game:HttpGet(repoUrl .. "main.lua"))()
 
 print("Bee Hub loaded! Use GUI to toggle features.")
